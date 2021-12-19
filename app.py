a3cb7cabebb3d82f9bebbc109c8c90c9877b3432
Chayno1 <chazmaster71@hotmail.co.uk>
2021-12-19 15:23:34 +0000

    added materialize and static files

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_films")
+def get_films():
+    films = list(mongo.db['films'].find({}))
+    users = list(mongo.db['users'].find({}))
+
+    print("FILMS: ", list(films))
+    print("USERS: ", list(users))
+    return render_template("base.html", films=films)
 
 
 if __name__ == "__main__":
