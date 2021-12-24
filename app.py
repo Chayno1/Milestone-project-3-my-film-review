b7c30c27bd167e962d802032e144169c25da6e55
Chayno1 <chazmaster71@hotmail.co.uk>
2021-12-24 17:16:16 +0000

    add search bar to search film titles

--- app.py
+++ app.py
@@ -27,6 +27,12 @@ def get_films():
     return render_template("home.html", films=films, reviews=reviews)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    films = list(mongo.db.films.find({"$text": {"$search": query}}))
+    return render_template("films.html", films=films)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
